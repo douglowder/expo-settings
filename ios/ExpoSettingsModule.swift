0ca2cbddcee7cccff159b53993aec3850ee3664a
Douglas Lowder <douglowder@mac.com>
2023-03-19 22:46:48 -0700

    Implementation for iOS

--- ios/ExpoSettingsModule.swift
+++ ios/ExpoSettingsModule.swift
@@ -4,14 +4,19 @@ public class ExpoSettingsModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoSettings")
 
-    Function("get") { (key: String) -> String in
-      "100"
+    Function("get") { (key: String) -> String? in
+      UserDefaults.standard.string(forKey: key)
     }
 
     Function("set") { (key: String, value: String) in
+      UserDefaults.standard.set(value, forKey: key)
+      UserDefaults.standard.synchronize()
     }
 
     Function("remove") { (key: String) in
+      UserDefaults.standard.removeObject(forKey: key)
+      UserDefaults.standard.synchronize()
     }
   }
 }
+
